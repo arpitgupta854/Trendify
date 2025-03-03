7acd56c2b1443a07f58d7a60ab12b71870880437
arpitgupta854 <arpitguptajalaun854@gmail.com>
2025-03-03 11:44:57 +0530

    The complete-projectt

---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Center from "./Center";
+import Footer from "./Footer";
+import Aboutme from "./Aboutme";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Center />
+      <Aboutme />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
